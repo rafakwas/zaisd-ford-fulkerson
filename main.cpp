1e202d4467de8648cb99036d3ea054761a951705
rafakwas <ralphigi@gmail.com>
2018-11-22 04:59:07 +0100

    init3

--- main.cpp
+++ main.cpp
@@ -6,10 +6,27 @@ using namespace std;
 #include <queue>
 #include<iostream>
 #include<list>
+#include <cmath>
 
-#define V 6
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include <vector>
 
-bool dfs(int rGraph[V][V], int x, int t, int * parent, bool * visited){
+int V;
+int E;
+
+int** alloc(int V) {
+    auto ** array = new int*[V];
+    for(int i = 0; i < V; ++i)
+    {
+        array[i] = new int[V];
+    }
+    return array;
+}
+
+
+bool dfs(int** rGraph, int x, int t, int * parent, bool * visited){
     if(x == t) return true;
     visited[x] = true;
     for(int i = 0; i < V; ++i){
@@ -22,7 +39,7 @@ bool dfs(int rGraph[V][V], int x, int t, int * parent, bool * visited){
     return false;
 }
 
-bool dfs(int rGraph[V][V], int s, int t, int parent[])
+bool dfs(int** rGraph, int s, int t, int parent[])
 {
     parent[s] = -1;
     bool visited[V];
@@ -30,7 +47,7 @@ bool dfs(int rGraph[V][V], int s, int t, int parent[])
     return dfs(rGraph, s, t, parent, visited);
 }
 
-int min_flow(int rGraph[V][V], int s, int t, int parent[]) {
+int min_flow(int **rGraph, int s, int t, const int parent[]) {
     int path_flow = INT_MAX;
     for (int v=t; v!=s; v=parent[v])
     {
@@ -40,9 +57,9 @@ int min_flow(int rGraph[V][V], int s, int t, int parent[]) {
     return path_flow;
 }
 
-int fordFulkerson(int graph[V][V], int s, int t)
+int fordFulkerson(int **graph, int s, int t)
 {
-    int rGraph[V][V];
+    int **rGraph = alloc(V);
     for (int u = 0; u < V; u++)
         for (int v = 0; v < V; v++)
             rGraph[u][v] = graph[u][v];
@@ -62,17 +79,44 @@ int fordFulkerson(int graph[V][V], int s, int t)
     return max_flow;
 }
 
-int main()
-{
-    int graph[V][V] = { {0, 16, 13, 0, 0, 0},
-                        {0, 0, 10, 12, 0, 0},
-                        {0, 4, 0, 0, 14, 0},
-                        {0, 0, 9, 0, 0, 20},
-                        {0, 0, 0, 7, 0, 4},
-                        {0, 0, 0, 0, 0, 0}
-    };
-    cout << "The maximum possible flow is " << fordFulkerson(graph, 0, 5);
-    return 0;
+
+int** read_matrix(char* file) {
+    ifstream myfile ("1.txt");
+    int **matrix = nullptr;
+    int vertice = 0;
+    string line;
+    if (myfile.is_open()) {
+        string item;
+        vector <string> splittedStrings;
+        int file_line = 0;
+        while (getline(myfile, line)) {
+            stringstream ss(line);
+            if (file_line == 0) {
+                V = atoi(line.c_str());
+            } else if (file_line == 1) {
+                E = atoi(line.c_str());
+                matrix = alloc(V);
+            } else {
+                while (getline(ss, item, ' ')) {
+                    splittedStrings.push_back(item);
+                }
+                for (int i = 0; i < splittedStrings.size(); ++i) {
+                    matrix[vertice][i] = stoi(splittedStrings.at(i));
+                }
+                splittedStrings.clear();
+                ++vertice;
+            }
+            ++file_line;
+        }
+        myfile.close();
+    } else cout << "Unable to open file";
+    return matrix;
 }
 
 
+int main(int argc, char* argv[])
+{
+    int **graph = read_matrix(argv[1]);
+    cout << "Maximum flow " << fordFulkerson(graph, 0, 3);
+    return 0;
+}
